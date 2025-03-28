47a6632a825cbf23bc14122e8d8fc801b10e2959
XaviXCC <xxwcn2014@gmail.com>
2025-03-28 11:49:24 -0400

    Update game.routes.js

--- server/routes/game.routes.js
+++ server/routes/game.routes.js
@@ -5,7 +5,7 @@ const gameController = require('../controllers/game.controller');
 // Open routes
 router.post('/addGame', gameController.addGame);
 router.get('/getGames', gameController.getGames);
-router.get('/getGameByID', gameController.getGameByID);
+router.get('/getGameByID', gameController.getGameById);
 router.put('/updateGame', gameController.updateGame);
 router.delete('/deleteGame', gameController.deleteGame);
 
