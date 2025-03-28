b220b923cfe3e0b26c94522602a855defd261c57
XaviXCC <xxwcn2014@gmail.com>
2025-03-28 12:07:08 -0400

    Update game.routes.js

--- server/routes/game.routes.js
+++ server/routes/game.routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const gameController = require('../controllers/game.controller');
+const authMiddleware = require('../security/verifyJWT');
 
+router.use(authMiddleware);
 // Open routes
 router.post('/addGame', gameController.addGame);
 router.get('/getGames', gameController.getGames);
