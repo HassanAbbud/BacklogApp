09b6ec9924e7d87f82a3402858635556eae7d3b3
XaviXCC <xxwcn2014@gmail.com>
2025-04-04 18:31:21 -0400

    SignInSignUp Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 //import Home from "./pages/Home";
-import Catalog from "./pages/Catalog"; 
+import Catalog from "./pages/Catalog";
+import SignInSignUp from "./pages/SignInSignUp";
 //import Account from "./pages/Account";
 //import NotFound from "./pages/NotFound"; 
 import Charts from "./pages/Charts";
@@ -14,12 +15,15 @@ function App() {
           <a href="/" className="btn red">Games</a>
           <a href="/catalog" className="btn blue">My Backlogs</a>
           <a href="/account" className="btn green">Account</a>
+          <a href="/charts" className="btn purple">Charts</a>
+          <a href="/signin" className="btn yellow">Sign In</a>
         </div>
-        
+
         <Routes>
           {/* <Route path="/" element={<Home />} /> */}
           <Route path="/catalog" element={<Catalog />} />
           <Route path="/charts" element={<Charts />} />
+          <Route path="/signin" element={<SignInSignUp />} />
           {/* <Route path="/account" element={<Account />} /> */}
           {/* <Route path="*" element={<NotFound />} /> */}
         </Routes>
