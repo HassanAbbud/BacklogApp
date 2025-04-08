ed4ab7e82054994ac1b3b7c8c95e819321e2a7c3
Hassan Sharif Abbud González <hassanabbud@hotmail.com>
2025-04-08 19:31:01 -0400

    Deleted unused dependencies and added CORS backend configuration.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "primereact/resources/themes/lara-light-indigo/theme.css"; // theme
-import "primereact/resources/primereact.min.css"; // core css
-import "primeicons/primeicons.css"; // icons
-import "primeflex/primeflex.css"; // PrimeFlex CSS utility library (optional)
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css"; 
+import "primeicons/primeicons.css"; 
+import "primeflex/primeflex.css"; 
 
 import App from './App.tsx'
 
